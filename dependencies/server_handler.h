50cd9291de56dc6f961c39379a2fec70f554b742
aj-codess <jagyei339@gmail.com>
2024-11-05 18:18:08 -0500

    progress

--- dependencies/server_handler.h
+++ dependencies/server_handler.h
@@ -17,10 +17,31 @@ class server_handler{
 
         cout<<"Server Initialised"<<endl;
 
+        this->endP_maker();
+
     };
 
     private:
     domain_dcl domain;
     boost::asio::io_context& ioc;
     boost::asio::ip::tcp::resolver addr_resolver;
+    boost::asio::ip::tcp::endpoint endpoint;
+    void endP_maker();
+};
+
+
+void server_handler::endP_maker(){
+
+    try{
+
+        boost::asio::ip::tcp::resolver::results_type resolved_endpoints=this->addr_resolver.async_resolve(this->domain.domain,this->domain.port,[&](boost::system::error_code& ec,boost::){
+
+        });
+
+    } catch(const std::exception& e){
+
+        cout<<"Error with endpoint - "<<e.what()<<endl;
+
+    };
+
 };
